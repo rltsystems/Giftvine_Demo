59270368f41b408723aa2f9cd38b283fe6edf751
IGramajoO <igramajo@outlook.com>
2022-03-03 02:35:10 -0800

    Working initial db

---
+++ src/test/java/com/example/cst438project2/Cst438Project2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cst438project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438Project2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
