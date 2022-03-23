46ad944e7d354071130ddefd10536d5bdb8cda38
Andrew Liddle <aliddle98@gmail.com>
2022-03-23 00:56:55 -0700

    - merged backend into Finishing_touches - added javadoc to Nacho's new calls

--- src/main/java/com/example/cst438project2/UserApi.java
+++ src/main/java/com/example/cst438project2/UserApi.java
@@ -35,7 +35,8 @@ public class UserApi {
      * @return
      */
     @PostMapping(path="/addUser")
-    public String addUser(@RequestParam String username, @RequestParam String password) {
+    public String addUser(@RequestParam String username, @RequestParam String password,
+                          @RequestParam (required = false) Boolean isAdmin) {
         User user = new User(username, password);
         if(isAdmin != null){
             user.setAdmin(isAdmin);
@@ -62,12 +63,18 @@ public class UserApi {
     @DeleteMapping(path="/deleteUser")
     public String deleteUser(@RequestParam String username){
         User user = new User(username, "");
-        user = findUserByUsername(username).get(0);
+        user = findUserByUsername(username).get(0); //maybe add an ispresent check
 
         userRepository.delete(user);
         return "Deleted";
     }
 
+    /**
+     * Logs User into their account
+     * @param username the string username chosen by the user
+     * @param password the string password that authenticates the user
+     * @return a success or error message
+     */
     @PostMapping(path="/login")
     public String login(@RequestParam String username, @RequestParam String password){
 
@@ -88,6 +95,14 @@ public class UserApi {
         }
     }
 
+    /**
+     * updates User information
+     * @param originalUsername the string of the initial username for searching the database
+     * @param newUsername the string new username to change out
+     * @param password the string password  used to authenticate
+     * @param isAdmin boolean indicator of admin status
+     * @return a success message
+     */
     @PatchMapping(path="/updateUser")
     public String updateUser(@RequestParam String originalUsername,
                              @RequestParam (required = false) String newUsername,
