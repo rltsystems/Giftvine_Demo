09b61a1ab3c96c9ebcab0629407169249b629e82
Andrew Liddle <aliddle98@gmail.com>
2022-03-23 00:00:33 -0700

    - added javadoc to WishlistApi.java

--- src/main/java/com/example/cst438project2/WishlistApi.java
+++ src/main/java/com/example/cst438project2/WishlistApi.java
@@ -18,16 +18,31 @@ public class WishlistApi {
     @Autowired
     UserRepository userRepository;
 
+    /**
+     * Retrieves all wishlists from the repository
+     * @return all WishLists currently stored
+     */
     @GetMapping(path="/allLists")
     public Iterable<Wishlist> getAllLists(){
         return wishlistRepository.findAll();
     }
 
+    /**
+     * Retrieves a specific Wishlist using its id number
+     * @param listId the integer id randomly generated for each Wishlist when it is stored in the repository
+     * @return the wishlist with a matching id number, if it exists
+     */
     @GetMapping(path="/list")
     public Optional<Wishlist> getWishlist(@RequestParam int listId){
         return wishlistRepository.findById(listId);
     }
 
+    /**
+     * Creates a new Wishlist and adds it to the repository as well as assigning it to the indicated User.
+     * @param username the string name of the user that the wishlist will be assigned to
+     * @param listName the string name of the list
+     * @return a message of a successful saving
+     */
     @PutMapping(path="/addList")
     public String addList(@RequestParam String username, @RequestParam String listName) {
         Wishlist wishList = new Wishlist(listName);
