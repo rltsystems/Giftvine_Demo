33f7b7284db9ec128bf863dd6ae4912f65464c9b
IGramajoO <igramajo@outlook.com>
2022-03-23 14:04:13 -0700

    added localstorage to track user being logged in. made it so page changes to profile when account is created

--- src/main/resources/static/assets/js/core/registerJs.js
+++ src/main/resources/static/assets/js/core/registerJs.js
@@ -1,5 +1,9 @@
 document.querySelector("#createAccBtn").addEventListener("click",createAccount);
 
+if(!("loggedInUser" in localStorage)){
+    localStorage.setItem("loggedInUser", null);
+}
+
 async function createAccount(){
     let username = document.querySelector("#username").value;
     let password = document.querySelector("#password").value;
@@ -20,6 +24,10 @@ async function createAccount(){
             .then(response => response.text())
             .then(result => console.log(result))
             .catch(error => console.log('error', error));
+
+        localStorage.setItem("loggedInUser", username);
+        console.log(localStorage.getItem("loggedInUser"));
+        document.location.href = `https://intense-springs-54966.herokuapp.com/profile.html`
     }
 }
 
