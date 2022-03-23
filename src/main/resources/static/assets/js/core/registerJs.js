0a38d549068054a5ce23e543f43095d0beac70f8
IGramajoO <igramajo@outlook.com>
2022-03-23 02:56:17 -0700

    implemented working POST api call to register user

--- src/main/resources/static/assets/js/core/registerJs.js
+++ src/main/resources/static/assets/js/core/registerJs.js
@@ -9,26 +9,17 @@ async function createAccount(){
         console.log("PASSWORDS DONT MATCH");
     }
     else{
-        let url = `https://intense-springs-54966.herokuapp.com/api/addUser`; //?username=${username}&password=${password}
-        let data = { username: username, password: password };
+        let url = `https://intense-springs-54966.herokuapp.com/api/addUser?username=${username}&password=${password}`;
 
-        await fetch(
-            url,
-            {
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(data),
-                method: "POST"
-            }
-        )
-            .then(data => data.json())
-            .then((json) => {
-                alert(JSON.stringify(json));
-            });
-        // let response = await fetchData(url, {
-        //         method: "POST"
-        //     });
-        // let data = await response.json();
-        // console.log(data)
+        const requestOptions = {
+            method: 'POST',
+            redirect: 'follow'
+        };
+
+        await fetch(url, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
     }
 }
 
