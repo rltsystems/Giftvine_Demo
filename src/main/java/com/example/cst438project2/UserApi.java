bde34a864df16d16f2aa83f3906b90d1b4f4a684
Andrew Liddle <aliddle98@gmail.com>
2022-03-23 00:09:09 -0700

    - added javadoc to UserApi.java

--- src/main/java/com/example/cst438project2/UserApi.java
+++ src/main/java/com/example/cst438project2/UserApi.java
@@ -17,11 +17,21 @@ public class UserApi {
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * Retrieves all currently stored users
+     * @return all User objects in userRepository
+     */
     @GetMapping(path="/allUsers")
     public Iterable<User> getAllUsers(){
         return userRepository.findAll();
     }
 
+    /**
+     * Creates a new user using a username and password, and stores it in the repository
+     * @param username the string chosen as the front end identifier for the user
+     * @param password the string password for the user account
+     * @return
+     */
     @PostMapping(path="/addUser")
     public String addUser(@RequestParam String username, @RequestParam String password) {
         User user = new User(username, password);
@@ -29,11 +39,21 @@ public class UserApi {
         return "saved";
     }
 
+    /**
+     * Searches repository for an existing user with the indicated username
+     * @param username the string used to search the database for a User
+     * @return the matching User object, if it exists
+     */
     @GetMapping(path="/findUserByUsername")
     public List<User> findUserByUsername(@RequestParam (defaultValue = "test") String username){
         return userRepository.findUserByUsername(username);
     }
 
+    /**
+     * Deletes a user from the repository using their username
+     * @param username the string used to search the database for a User
+     * @return a success message
+     */
     @DeleteMapping(path="/deleteUser")
     public String deleteUser(@RequestParam String username){
         User user = new User(username, "");
